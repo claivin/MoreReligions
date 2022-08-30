c392ea4fb84c70e0024508f22183763848ef47d1
nitr0gue14 <87732208+nitr0gue14@users.noreply.github.com>
2022-08-30 17:57:38 -0400

    Buddhism and Dharmic

---
+++ common/defines/00_mrdefines.lua
@@ -0,0 +1,8 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+NDefines.NReligion.KARMA_FOR_OFFENSIVE_WAR = -15
+NDefines.NReligion.KARMA_FOR_HONORING_CTA = 25
+NDefines.NReligion.KARMA_PER_RELEASED_PROVINCE = 1
+NDefines.NReligion.KARMA_PER_TAKEN_PROVINCE = -0
